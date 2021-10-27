a3a3ed234837ca9e674b601d11c0788cb81232e4
Dylan <dylan@namtech.sg>
2021-10-27 18:52:43 +0700

    update auth business

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,8 @@ const authBusiness = require("../businesses/authBusiness");
 const validateModel = require("../middleware/validateModel");
 const { userValidate } = require("../validateModel/user");
 
+router.post("/auths", authBusiness.verifyToken);
+
 router.post("/auths/register", validateModel(userValidate.userCreate), authBusiness.register);
 
 router.post("/auths/login", validateModel(userValidate.userLogin), authBusiness.login);
