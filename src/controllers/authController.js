10f989375bd9b22becfb8b290ce3094f68367373
Dylan <dylan@namtech.sg>
2021-10-27 17:49:52 +0700

    update auth path controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,10 +3,10 @@ const authBusiness = require("../businesses/authBusiness");
 const validateModel = require("../middleware/validateModel");
 const { userValidate } = require("../validateModel/user");
 
-router.post("auths/register", validateModel(userValidate.userCreate), authBusiness.register);
+router.post("/auths/register", validateModel(userValidate.userCreate), authBusiness.register);
 
-router.post("auths/login", validateModel(userValidate.userLogin), authBusiness.login);
+router.post("/auths/login", validateModel(userValidate.userLogin), authBusiness.login);
 
-router.post("auths/verify-capcha", authBusiness.verifyCapcha);
+router.post("/auths/verify-capcha", authBusiness.verifyCapcha);
 
 module.exports = router;
