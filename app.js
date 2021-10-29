23076501d78f8a1aed6ac1d024f589b4ac3e6550
Dylan <lqvinh243@gmail.com>
2021-10-29 18:39:40 +0700

    update cluster

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const server = require("./src/server");
+const cluster = require("cluster");
+const { cpus } = require("os");
+const process = require("process");
+const numCPUs = cpus().length;
 
-server.start(() => { console.log("Service api start"); });
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+
+    cluster.on("exit", (worker, code, signal) => {
+      i--;
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  }
+} else {
+  server.start(() => { console.log("Service api start"); });
+
+  console.log(`Worker ${process.pid} started`);
+}
